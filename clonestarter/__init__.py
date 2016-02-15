1eceb291f1a0d717cf0b395dc7d00aee3071290c
ahadsheriff <ahadsheriff@gmail.com>
2016-02-15 00:47:04 -0500

    Set up the front end
    Used Bootstrap framework to set up the main html and css structure

--- clonestarter/__init__.py
+++ clonestarter/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.script import Manager
 
 app = Flask(__name__)
@@ -6,4 +6,4 @@ manager = Manager(app)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
