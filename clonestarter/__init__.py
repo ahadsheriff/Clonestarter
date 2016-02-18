aad8f0b7a29f0345cfd9fa99476b513a638d68ce
ahadsheriff <ahadsheriff@gmail.com>
2016-02-18 00:20:28 -0500

    Finished setting up database and SQLAlchemy
    Migrations and other things.

--- clonestarter/__init__.py
+++ clonestarter/__init__.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
 
 app = Flask(__name__)
+app.config.from_object('clonestarter.default_settings')
 manager = Manager(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+from clonestarter.models import *
+
 
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+
+@app.route("/projects/create/")
+def create():
+    return render_template("create.html")
