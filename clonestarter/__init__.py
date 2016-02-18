4cb695aa1b786596ffb82d0d56bd81a3f620a796
ahadsheriff <ahadsheriff@gmail.com>
2016-02-18 16:11:26 -0500

    Built the create project view

--- clonestarter/__init__.py
+++ clonestarter/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
+import datetime
 
 app = Flask(__name__)
 app.config.from_object('clonestarter.default_settings')
@@ -16,9 +17,46 @@ from clonestarter.models import *
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+	return render_template("index.html")
 
 
-@app.route("/projects/create/")
+@app.route('/projects/create/', methods=['GET', 'POST'])
 def create():
-    return render_template("create.html")
+	if request.method == "GET":
+		return render_template('create.html')
+
+	if request.method == "POST":
+		#Handle the form submission
+
+		now = datetime.datetime.now()
+		time_end = request.form.get("funding_end_date")
+		time_end = datetime.datetime.strptime(time_end, "%Y-%m-%d")
+
+		new_project = Project(
+			member_id = 1, #Guest Creator
+			name = request.form.get("project_name"),
+			short_description = request.form.get("short_description"),
+			long_description = request.form.get("long_description"),
+			goal_amount = request.form.get("funding_goal"),
+			time_start = now,
+			time_end = time_end,
+			time_created = now
+		)
+
+		db.session.add(new_project)
+		db.session.commit()
+
+		return redirect(url_for('create'))	
+
+
+
+
+
+
+
+
+
+
+
+
+
