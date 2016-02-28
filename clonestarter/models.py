46d349b5347426885b575926624b2f0801f7901b
ahadsheriff <ahadsheriff@gmail.com>
2016-02-28 14:37:16 -0500

    Finished building the project detail page
    Now when a user creates a new project, they are directed to their
    project page where they can view how they are doing on their
    crowdfunding campaign

--- clonestarter/models.py
+++ clonestarter/models.py
@@ -1,4 +1,7 @@
 from clonestarter import db
+from sqlalchemy.sql import func
+import datetime
+
 
 
 class Member(db.Model):
@@ -19,6 +22,26 @@ class Project(db.Model):
     time_end = db.Column(db.DateTime)
     time_created = db.Column(db.DateTime)
     pledges = db.relationship('Pledge', backref='project', foreign_keys='Pledge.project_id')
+
+    @property 
+    def num_pledges(self):
+        return len(self.pledges)
+
+    @property 
+    def total_pledges(self):
+        total_pledges = db.session.query(func.sum(Pledge.amount)).filter(Pledge.project_id == self.id).one()[0]
+        if total_pledges is None:
+            total_pledges = 0
+
+        return total_pledges
+
+    @property 
+    def num_days_left(self):
+        now = datetime.datetime.now()
+        num_days_left = (self.time_end - now).days
+
+        return num_days_left
+
 
 class Pledge(db.Model):
     id = db.Column(db.Integer, primary_key=True)
